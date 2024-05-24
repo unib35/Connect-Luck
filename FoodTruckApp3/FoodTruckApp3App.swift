e726676497af5bf5c70566eb664beb99fb2aee36
Jong MIn <unib335@naver.com>
2024-05-24 17:30:46 +0900

    Initial Commit

---
+++ FoodTruckApp3/FoodTruckApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTruckApp3App.swift
+//  FoodTruckApp3
+//
+//  Created by 이종민 on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodTruckApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodTruckApp3UITests/FoodTruckApp3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FoodTruckApp3UITestsLaunchTests.swift
+//  FoodTruckApp3UITests
+//
+//  Created by 이종민 on 5/24/24.
+//
+
+import XCTest
+
+final class FoodTruckApp3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
